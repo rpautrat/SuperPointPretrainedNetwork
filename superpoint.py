9bb5d65a45bcd5e93ffbd01bcded9763bd128436
Rémi Pautrat <remi.pautrat@polytechnique.org>
2020-07-30 15:31:04 +0200

    SuperPoint model only with online homography adaptation

---
+++ superpoint.py
@@ -0,0 +1,368 @@
+#!/usr/bin/env python
+#
+# %BANNER_BEGIN%
+# ---------------------------------------------------------------------
+# %COPYRIGHT_BEGIN%
+#
+#  Magic Leap, Inc. ("COMPANY") CONFIDENTIAL
+#
+#  Unpublished Copyright (c) 2018
+#  Magic Leap, Inc., All Rights Reserved.
+#
+# NOTICE:  All information contained herein is, and remains the property
+# of COMPANY. The intellectual and technical concepts contained herein
+# are proprietary to COMPANY and may be covered by U.S. and Foreign
+# Patents, patents in process, and are protected by trade secret or
+# copyright law.  Dissemination of this information or reproduction of
+# this material is strictly forbidden unless prior written permission is
+# obtained from COMPANY.  Access to the source code contained herein is
+# hereby forbidden to anyone except current COMPANY employees, managers
+# or contractors who have executed Confidentiality and Non-disclosure
+# agreements explicitly covering such access.
+#
+# The copyright notice above does not evidence any actual or intended
+# publication or disclosure  of  this source code, which includes
+# information that is confidential and/or proprietary, and is a trade
+# secret, of  COMPANY.   ANY REPRODUCTION, MODIFICATION, DISTRIBUTION,
+# PUBLIC  PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS
+# SOURCE CODE  WITHOUT THE EXPRESS WRITTEN CONSENT OF COMPANY IS
+# STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE LAWS AND
+# INTERNATIONAL TREATIES.  THE RECEIPT OR POSSESSION OF  THIS SOURCE
+# CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS
+# TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE,
+# USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+#
+# %COPYRIGHT_END%
+# ----------------------------------------------------------------------
+# %AUTHORS_BEGIN%
+#
+#  Originating Authors: Daniel DeTone (ddetone)
+#                       Tomasz Malisiewicz (tmalisiewicz)
+#
+# %AUTHORS_END%
+# --------------------------------------------------------------------*/
+# %BANNER_END%
+
+
+import argparse
+import glob
+import numpy as np
+import os
+import time
+
+import cv2
+import torch
+import torch.nn.functional as F
+from kornia.geometry.transform import warp_perspective
+
+from ...datasets.utils.homographies import sample_homography
+
+# Stub to warn about opencv version.
+if int(cv2.__version__[0]) < 3: # pragma: no cover
+  print('Warning: OpenCV 3 is not installed')
+
+# Jet colormap for visualization.
+myjet = np.array([[0.        , 0.        , 0.5       ],
+                  [0.        , 0.        , 0.99910873],
+                  [0.        , 0.37843137, 1.        ],
+                  [0.        , 0.83333333, 1.        ],
+                  [0.30044276, 1.        , 0.66729918],
+                  [0.66729918, 1.        , 0.30044276],
+                  [1.        , 0.90123457, 0.        ],
+                  [1.        , 0.48002905, 0.        ],
+                  [0.99910873, 0.07334786, 0.        ],
+                  [0.5       , 0.        , 0.        ]])
+
+class SuperPointNet(torch.nn.Module):
+  """ Pytorch definition of SuperPoint Network. """
+  def __init__(self):
+    super(SuperPointNet, self).__init__()
+    self.relu = torch.nn.ReLU(inplace=True)
+    self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+    c1, c2, c3, c4, c5, d1 = 64, 64, 128, 128, 256, 256
+    # Shared Encoder.
+    self.conv1a = torch.nn.Conv2d(1, c1, kernel_size=3, stride=1, padding=1)
+    self.conv1b = torch.nn.Conv2d(c1, c1, kernel_size=3, stride=1, padding=1)
+    self.conv2a = torch.nn.Conv2d(c1, c2, kernel_size=3, stride=1, padding=1)
+    self.conv2b = torch.nn.Conv2d(c2, c2, kernel_size=3, stride=1, padding=1)
+    self.conv3a = torch.nn.Conv2d(c2, c3, kernel_size=3, stride=1, padding=1)
+    self.conv3b = torch.nn.Conv2d(c3, c3, kernel_size=3, stride=1, padding=1)
+    self.conv4a = torch.nn.Conv2d(c3, c4, kernel_size=3, stride=1, padding=1)
+    self.conv4b = torch.nn.Conv2d(c4, c4, kernel_size=3, stride=1, padding=1)
+    # Detector Head.
+    self.convPa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+    self.convPb = torch.nn.Conv2d(c5, 65, kernel_size=1, stride=1, padding=0)
+    # Descriptor Head.
+    self.convDa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+    self.convDb = torch.nn.Conv2d(c5, d1, kernel_size=1, stride=1, padding=0)
+
+  def forward(self, x):
+    """ Forward pass that jointly computes unprocessed point and descriptor
+    tensors.
+    Input
+      x: Image pytorch tensor shaped N x 1 x H x W.
+    Output
+      heatmap: Output point pytorch tensor shaped N x 1 x H x W.
+      desc: Output descriptor pytorch tensor shaped N x 256 x H/8 x W/8.
+    """
+    # Shared Encoder.
+    x = self.relu(self.conv1a(x))
+    x = self.relu(self.conv1b(x))
+    x = self.pool(x)
+    x = self.relu(self.conv2a(x))
+    x = self.relu(self.conv2b(x))
+    x = self.pool(x)
+    x = self.relu(self.conv3a(x))
+    x = self.relu(self.conv3b(x))
+    x = self.pool(x)
+    x = self.relu(self.conv4a(x))
+    x = self.relu(self.conv4b(x))
+    # Detector Head.
+    cPa = self.relu(self.convPa(x))
+    semi = self.convPb(cPa)
+    heatmap = F.softmax(semi, dim=1)[:, :-1]
+    heatmap = F.pixel_shuffle(heatmap, 8)
+    # Descriptor Head.
+    cDa = self.relu(self.convDa(x))
+    desc = self.convDb(cDa)
+    dn = torch.norm(desc, p=2, dim=1) # Compute the norm.
+    desc = desc.div(torch.unsqueeze(dn, 1)) # Divide by norm to normalize.
+    return heatmap, desc
+
+
+class SuperPointFrontend(object):
+  """ Wrapper around pytorch net to help with pre and post image processing. """
+  def __init__(self, weights_path, conf, nms_dist, conf_thresh, nn_thresh,
+               ha=False, cuda=False):
+    self.name = 'SuperPoint'
+    self.cuda = cuda
+    self.nms_dist = nms_dist
+    self.conf_thresh = conf_thresh
+    self.nn_thresh = nn_thresh # L2 descriptor distance for good match.
+    self.cell = 8 # Size of each output cell. Keep this fixed.
+    self.border_remove = 4 # Remove points this close to the border.
+    self.conf = conf
+    self.ha = ha
+
+    # Load the network in inference mode.
+    self.net = SuperPointNet()
+    if cuda:
+      # Train on GPU, deploy on GPU.
+      self.net.load_state_dict(torch.load(weights_path))
+      self.net = self.net.cuda()
+    else:
+      # Train on GPU, deploy on CPU.
+      self.net.load_state_dict(torch.load(weights_path,
+                               map_location=lambda storage, loc: storage))
+    self.net.eval()
+
+  def nms_fast(self, in_corners, H, W, dist_thresh):
+    """
+    Run a faster approximate Non-Max-Suppression on numpy corners shaped:
+      3xN [x_i,y_i,conf_i]^T
+  
+    Algo summary: Create a grid sized HxW. Assign each corner location a 1, rest
+    are zeros. Iterate through all the 1's and convert them either to -1 or 0.
+    Suppress points by setting nearby values to 0.
+  
+    Grid Value Legend:
+    -1 : Kept.
+     0 : Empty or suppressed.
+     1 : To be processed (converted to either kept or supressed).
+  
+    NOTE: The NMS first rounds points to integers, so NMS distance might not
+    be exactly dist_thresh. It also assumes points are within image boundaries.
+  
+    Inputs
+      in_corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
+      H - Image height.
+      W - Image width.
+      dist_thresh - Distance to suppress, measured as an infinty norm distance.
+    Returns
+      nmsed_corners - 3xN numpy matrix with surviving corners.
+      nmsed_inds - N length numpy vector with surviving corner indices.
+    """
+    grid = np.zeros((H, W)).astype(int) # Track NMS data.
+    inds = np.zeros((H, W)).astype(int) # Store indices of points.
+    # Sort by confidence and round to nearest int.
+    inds1 = np.argsort(-in_corners[2,:])
+    corners = in_corners[:,inds1]
+    rcorners = corners[:2,:].round().astype(int) # Rounded corners.
+    # Check for edge case of 0 or 1 corners.
+    if rcorners.shape[1] == 0:
+      return np.zeros((3,0)).astype(int), np.zeros(0).astype(int)
+    if rcorners.shape[1] == 1:
+      out = np.vstack((rcorners, in_corners[2])).reshape(3,1)
+      return out, np.zeros((1)).astype(int)
+    # Initialize the grid.
+    for i, rc in enumerate(rcorners.T):
+      grid[rcorners[1,i], rcorners[0,i]] = 1
+      inds[rcorners[1,i], rcorners[0,i]] = i
+    # Pad the border of the grid, so that we can NMS points near the border.
+    pad = dist_thresh
+    grid = np.pad(grid, ((pad,pad), (pad,pad)), mode='constant')
+    # Iterate through points, highest to lowest conf, suppress neighborhood.
+    count = 0
+    for i, rc in enumerate(rcorners.T):
+      # Account for top and left padding.
+      pt = (rc[0]+pad, rc[1]+pad)
+      if grid[pt[1], pt[0]] == 1: # If not yet suppressed.
+        grid[pt[1]-pad:pt[1]+pad+1, pt[0]-pad:pt[0]+pad+1] = 0
+        grid[pt[1], pt[0]] = -1
+        count += 1
+    # Get all surviving -1's and return sorted array of remaining corners.
+    keepy, keepx = np.where(grid==-1)
+    keepy, keepx = keepy - pad, keepx - pad
+    inds_keep = inds[keepy, keepx]
+    out = corners[:, inds_keep]
+    values = out[-1, :]
+    inds2 = np.argsort(-values)
+    out = out[:, inds2]
+    out_inds = inds1[inds_keep[inds2]]
+    return out, out_inds
+
+  def erode(self, img, kernel_size=3):
+    """ Erode a binary Tensor image of shape (B, H, W)
+        given a specific kernel size. Odd kernel size supported only. """
+    assert (kernel_size % 2) == 1
+
+    # Pad the image
+    pad = kernel_size // 2
+    padded_img = F.pad(img, (pad,) * 4, mode='constant', value=1)
+
+    # Min pool
+    padded_img = -padded_img.unsqueeze(1)
+    output = -F.max_pool2d(padded_img, kernel_size, stride=1).squeeze(1)
+    return output
+
+  def homographic_adaptation(self, image, conf):
+    """ Perform batched homographic adaptation on a batched image.
+
+    Arguments:
+        image: A (b_size, 1, H, W) image Tensor.
+
+    Returns:
+        The batched aggregated detection probability maps (b_size, 1, H, W).
+    """
+    b_size, _, h, w = image.shape
+    mb_size = conf['mini_batch_size']
+    n_H = conf['num_homographies']
+    n_iter = n_H // mb_size
+    device = image.device
+
+    probs = torch.empty(b_size, 0, h, w, dtype=torch.float, device=device)
+    counts = torch.empty(b_size, 0, h, w, dtype=torch.float, device=device)
+
+    for i in range(n_iter):
+      # Generate random homographies
+      H = [sample_homography((h, w), **conf['homographies'])[0]
+           for _ in range(mb_size)]
+      if i == 0:
+        # The first homography is the identity
+        H[0] = np.eye(3)
+      H = np.stack(H, axis=0)
+      H_inv = np.linalg.inv(H)
+      H = torch.tensor(H, dtype=torch.float,
+                       device=device).repeat(b_size, 1, 1)
+      H_inv = torch.tensor(H_inv, dtype=torch.float,
+                           device=device).repeat(b_size, 1, 1)
+      # H is [H0, H1, H2, H0, H1, H2, ...]
+
+      # Batch the images
+      images = image.repeat_interleave(mb_size, dim=0)
+      # images is [img0, img0, img0, img1, img1, img1, ...]
+
+      # Warp all the images at the probability map size
+      warped_img = warp_perspective(images, H, (h, w))
+      count = torch.ones(b_size * mb_size, 1, h, w,
+                         dtype=torch.float, device=device)
+      warped_count = warp_perspective(count, H, (h, w),
+                                      flags='nearest').squeeze(1)
+
+      # Erode the count to ignore detections on the boundary
+      warped_count = self.erode(warped_count, kernel_size=3)
+
+      # Run the network on the warped images and mask them
+      warped_prob = self.net(warped_img)[0] * warped_count
+
+      # Warp back and aggregate the results
+      prob = warp_perspective(warped_prob, H_inv, (h, w))
+      probs = torch.cat([probs, prob.reshape(b_size, mb_size, h, w)], dim=1)
+      count = warp_perspective(warped_count.unsqueeze(1), H_inv, (h, w))
+      counts = torch.cat([counts, count.reshape(b_size, mb_size, h, w)],
+                          dim=1)
+    
+    if conf['aggregation'] == 'max':
+      prob = torch.max(probs, dim=1)[0]
+    else:
+      prob = (probs * counts).sum(dim=1)
+      count = torch.clamp(counts, min=1).sum(dim=1)
+      prob /= count
+    return prob.unsqueeze(1)
+
+  def run(self, img):
+    """ Process a numpy image to extract points and descriptors.
+    Input
+      img - HxW numpy float32 input image in range [0,1].
+    Output
+      corners - 3xN numpy array with corners [x_i, y_i, confidence_i]^T.
+      desc - 256xN numpy array of corresponding unit normalized descriptors.
+      heatmap - HxW numpy heatmap in range [0,1] of point confidences.
+      """
+    assert img.ndim == 2, 'Image must be grayscale.'
+    assert img.dtype == np.float32, 'Image must be float32.'
+    orig_H, orig_W = img.shape[:2]
+    H, W = orig_H - (orig_H % 8), orig_W - (orig_W % 8)  # prev multiple of 8
+    scale_H, scale_W = orig_H / H, orig_W / W
+    inp = cv2.resize(img, (W, H)).reshape(1, H, W)
+    inp = torch.from_numpy(inp)
+    inp = torch.autograd.Variable(inp).view(1, 1, H, W)
+    if self.cuda:
+      inp = inp.cuda()
+    # Forward pass of network.
+    outs = self.net.forward(inp)
+    heatmap, coarse_desc = outs[0], outs[1]
+    if self.ha:
+      # Homography adaptation
+      heatmap = self.homographic_adaptation(inp, self.conf)
+    # Convert pytorch -> numpy.
+    heatmap = heatmap.data.cpu().numpy().squeeze()
+    # --- Process points.
+    xs, ys = np.where(heatmap >= self.conf_thresh) # Confidence threshold.
+    if len(xs) == 0:
+      return np.zeros((3, 0)), None, None
+    pts = np.zeros((3, len(xs))) # Populate point data sized 3xN.
+    pts[0, :] = ys
+    pts[1, :] = xs
+    pts[2, :] = heatmap[xs, ys]
+    pts, _ = self.nms_fast(pts, H, W, dist_thresh=self.nms_dist) # Apply NMS.
+    inds = np.argsort(pts[2,:])
+    pts = pts[:,inds[::-1]] # Sort by confidence.
+    # Remove points along border.
+    bord = self.border_remove
+    toremoveW = np.logical_or(pts[0, :] < bord, pts[0, :] >= (W-bord))
+    toremoveH = np.logical_or(pts[1, :] < bord, pts[1, :] >= (H-bord))
+    toremove = np.logical_or(toremoveW, toremoveH)
+    pts = pts[:, ~toremove]
+    # --- Process descriptor.
+    D = coarse_desc.shape[1]
+    if pts.shape[1] == 0:
+      desc = np.zeros((D, 0))
+    else:
+      # Interpolate into descriptor map using 2D point locations.
+      samp_pts = torch.from_numpy(pts[:2, :].copy())
+      samp_pts[0, :] = (samp_pts[0, :] / (float(W)/2.)) - 1.
+      samp_pts[1, :] = (samp_pts[1, :] / (float(H)/2.)) - 1.
+      samp_pts = samp_pts.transpose(0, 1).contiguous()
+      samp_pts = samp_pts.view(1, 1, -1, 2)
+      samp_pts = samp_pts.float()
+      if self.cuda:
+        samp_pts = samp_pts.cuda()
+      desc = torch.nn.functional.grid_sample(coarse_desc, samp_pts)
+      desc = desc.data.cpu().numpy().reshape(D, -1)
+      desc /= np.linalg.norm(desc, axis=0)[np.newaxis, :]
+    # Adapt the points and heatmap to the original image size
+    pts[0, :] *= scale_W
+    pts[1, :] *= scale_H
+    heatmap = cv2.resize(heatmap, (orig_W, orig_H))
+    return pts, desc, heatmap
